f3d6f53b7c97433623cf1f89d6eb6b25f4e73b51
knair <knair@alarm.com>
2023-03-30 10:28:10 -0400

    basic PWA

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Challenge(){
+  const [result, setResult] = useState();
+
+  async function onSubmit(event){
+    event.preventDefault();
+    // create a list of challenges
+    const challenges = [
+      "Read for 10 minutes",
+      "Take a walk!",
+      "Focus on your breating and meditate for 5 minutes"
+    ]
+    setResult(challenges[Math.floor(Math.random()*challenges.length)]);
+  }
+  return ( 
+    <div className='challenge'>
+      <h1>Try this challenge today: </h1>
+        <button onClick={onSubmit}>Generate Challenge</button>
+      <p>{result}</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Daily Challenge</header>
+      <Challenge/>
     </div>
   );
 }
